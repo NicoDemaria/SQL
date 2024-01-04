c28b303da4c8cf2cda1e8df1d2157d3480a38795
Nicolas <nicolasdemaria4@gmail.com>
2024-01-04 12:14:29 -0300

    Update Assessment Test # 2.sql

--- Assessment Test # 2.sql
+++ Assessment Test # 2.sql
@@ -29,41 +29,84 @@
 -- Expected Result is 2 rows
 
 
--- How can you produce a list of members who joined after the start of September 2012? Return the memid, surname, firstname, and joindate of the members in question.
--- Expected Result is 10 rows (not all are shown below)
+-- How can you produce a list of members who joined after the start of September 
+--2012? Return the memid, surname, firstname, and joindate of the members in question.
+-- Expected Result is 10 rows (not all are shown below
 
+select * from cd.members
+WHERE joindate >= '2012-09-01'
 
--- How can you produce an ordered list of the first 10 surnames in the members table? The list must not contain duplicates.
+-- How can you produce an ordered list of the first 10 surnames in the members table? 
+-- The list must not contain duplicates.
 -- Expected Result should be 10 rows if you include GUEST as a last name
 
-
+SELECT distinct surname 
+FROM cd.members
+ORDER BY surname asc
+limit 10
 
 -- You'd like to get the signup date of your last member. How can you retrieve this information?
 -- Expected Result
 -- 2012-09-26 18:08:45
+SELECT  joindate 
+FROM cd.members
+ORDER BY joindate desc
+limit 1
 
 -- Produce a count of the number of facilities that have a cost to guests of 10 or more.
 -- Expected Result
 -- 6
 
+SELECT count(*) FROM cd.facilities
+WHERE guestcost >= 10
+
 -- Produce a list of the total number of slots booked per facility in the month of September 2012. Produce an output table consisting of facility id and slots, sorted by the number of slots.
 -- Expected Result is 9 rows
 
+SELECT b.facid, sum(slots) 
+FROM cd.bookings b
+WHERE b.starttime BETWEEN '2012-09-01' AND '2012-10-01'
+group by b.facid
 
--- Produce a list of facilities with more than 1000 slots booked. Produce an output table consisting of facility id and total slots, sorted by facility id.
+
+
+
+-- Produce a list of facilities with more than 1000 slots booked. 
+-- Produce an output table consisting of facility id and total slots, sorted by facility id.
 -- Expected Result is 5 rows
 
+SELECT facid, sum(slots)
+FROM cd.bookings
+GROUP BY facid
+HAVING sum(slots) >= 1000
 
--- How can you produce a list of the start times for bookings for tennis courts, for the date '2012-09-21'? Return a list of start time and facility name pairings, ordered by the time.
+
+
+
+-- How can you produce a list of the start times for bookings for tennis courts,
+-- for the date '2012-09-21'? Return a list of start time and facility name pairings, ordered by the time.
 -- Expected Result is 12 rows
 
+
+SELECT b.starttime, f.name
+FROM cd.facilities f
+LEFT JOIN cd.bookings b
+ON f.facid = b.facid 
+WHERE f.name like '%Tennis Court%' AND b.starttime BETWEEN '2012-09-21' AND '2012-09-22'
+ORDER BY b.starttime
 
 
 -- How can you produce a list of the start times for bookings by members named 'David Farrell'?
 -- Expected result is 34 rows of timestamps
+SELECT b.starttime, m.surname || ' ' || m.firstname
+FROM cd.members m
+LEFT JOIN cd.bookings b
+ON m.memid = b.memid
+WHERE m.surname = 'Farrell' AND m.firstname = 'David'
 
 
 
-SELECT * from cd.facilities
-
+select * from cd.facilities
+select * from cd.bookings
+select * from cd.members
 
